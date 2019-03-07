f0203243b46310846fb4729aece42a578337d2a8
Sungwook Kim <discerningacumen@gmail.com>
2019-03-08 01:24:21 +0900

    The original functions in generic forms which had been created by me are added again in order to update these functions correctly in future.

--- smoothstep/smoothstep.playground/Contents.swift
+++ smoothstep/smoothstep.playground/Contents.swift
@@ -22,6 +22,40 @@ func clamp(_ x:Double,_ min: Double,_ max: Double) throws -> Double {
     return x
 }
 
+/*
+ 
+ // Both functions originally created in generic form.
+ // The issue was a conversion from T into Double.
+ 
+ func clamp<T: FloatingPoint>(_ x:T,_ min: T,_ max: T) throws -> T {
+ 
+     guard max > min else {
+        throw Math_Error.E_MINMAX_REVERSED
+     }
+     if x < min {
+        return min
+     } else if x > max {
+        return max
+     }
+ 
+     return x
+ }
+ 
+ 
+func smoothstep<T: FloatingPoint>((_ edge0:T,_ edge1:T,_ x:T) throws -> T {
+
+    guard edge0 != edge1 else {
+        throw Math_Error.E_OVERFLOW
+    }
+
+    let t = try clamp((x-edge0)/(edge1-edge0), 0.0, 1.0)
+
+    return t*t*(3.0 - 2.0*t) // -> conversion T into double was not specified!
+
+    }
+*/
+
+
 func smoothstep(_ edge0:Double,_ edge1:Double,_ x:Double) throws -> Double {
     
     guard edge0 != edge1 else {
